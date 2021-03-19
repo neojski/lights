7bc51eadbb97f6769542429d476c22976be71a0c
Tomasz Kołodziejski <tkolodziejski@gmail.com>
2021-03-19 21:36:56 +0000

    Minor cleanup

--- ColorPalette/ColorPalette.ino
+++ ColorPalette/ColorPalette.ino
@@ -5,17 +5,16 @@
 #define BRIGHTNESS  64
 #define LED_TYPE    WS2811
 #define COLOR_ORDER RGB
+#define UPDATES_PER_SECOND 100
 CRGB leds[NUM_LEDS];
 
-#define UPDATES_PER_SECOND 100
-
-CRGBPalette16 currentPalette;
-TBlendType    currentBlending;
-
-extern CRGBPalette16 myRedWhiteBluePalette;
-extern const TProgmemPalette16 myRedWhiteBluePalette_p PROGMEM;
-
 const int potentiometer = A0;
+
+char buffer[1000];
+void printColor (CRGB c) {
+  sprintf(buffer, "(%d, %d, %d)", c[0], c[1], c[2]);
+  Serial.print(buffer);
+}
 
 void setup() {
   Serial.begin(9600); // open the serial port at 9600 bps:
@@ -26,13 +25,12 @@ void setup() {
   // There's no second GND so let's make D2 low
   pinMode(4, OUTPUT);
   digitalWrite(4, LOW);
-
-  currentPalette = RainbowColors_p;
-  currentBlending = LINEARBLEND;
 }
 
+// Read from potentiometer and smooth the output.
+// Returns a value in [0, 1]
 float knob() {
-  float p = analogRead(potentiometer) / 1024.;
+  const float p = analogRead(potentiometer) / 1024.;
   // clamp at 10 as it seems my lights min:
 
   static float p_smooth = 0.;
@@ -41,39 +39,32 @@ float knob() {
   return p_smooth;
 }
 
-void loop()
-{
+void loop() {
   static int time = 0;
   time = time + 16; /* motion speed */
 
-  float p = knob();
+  const float p = knob();
 
   if (p >= 0.8) {
     // rainbow
-    FillLEDsFromPaletteColors(time, p);
+    FillLEDsFromPaletteColors(time);
   } else {
     // normal white
-    warmWhite(p);
+    dimmableWhite(p);
   }
 
   FastLED.show();
   FastLED.delay(1000 / UPDATES_PER_SECOND);
 }
 
-char buffer[1000];
-void printColor (CRGB c) {
-  sprintf(buffer, "(%d, %d, %d)", c[0], c[1], c[2]);
-  Serial.print(buffer);
-}
-
-void warmWhite(float knob) {
+void dimmableWhite(float knob) {
   int howManyOn = NUM_LEDS;
   const float turnOffThreshold = 0.5;
   if (knob < turnOffThreshold) {
     howManyOn = NUM_LEDS * (knob / turnOffThreshold);
   }
 
-  uint8_t brightness = 32 + knob * 32; // below roughly 32 the LEDs colours get some ugly tint 
+  const uint8_t brightness = 32 + knob * (255 - 32); // below roughly 32 the LEDs colours get some ugly tint
   FastLED.setBrightness(brightness);
   for (int i = 0; i < NUM_LEDS; i++) {
     if (i < howManyOn) {
@@ -84,24 +75,17 @@ void warmWhite(float knob) {
   }
 }
 
-void FillLEDsFromPaletteColors(int time, float p)
-{
-  uint8_t brightness = 255 * p;
-  // clamp at 10 as it seems hardware min for these lights
-  if (brightness < 10) {
-    brightness = 10;
-  }
-
+void FillLEDsFromPaletteColors(int time) {
+  const uint8_t brightness = 255;
   for ( int i = 0; i < NUM_LEDS; i++) {
-    uint8_t paletteIndex = (time / 256) % 256;
-    uint8_t subPaletteIndex = time % 256;
+    const uint8_t paletteIndex = (time / 256) % 256;
+    const uint8_t subPaletteIndex = time % 256;
 
     leds[i] =
       blend(
-        ColorFromPalette( currentPalette, paletteIndex, brightness, currentBlending),
-        ColorFromPalette( currentPalette, (paletteIndex + 1) % 256, brightness, currentBlending),
+        ColorFromPalette( RainbowColors_p, paletteIndex, brightness, LINEARBLEND),
+        ColorFromPalette( RainbowColors_p, (paletteIndex + 1) % 256, brightness, LINEARBLEND),
         subPaletteIndex);
     time += 256;
   }
-
 }
